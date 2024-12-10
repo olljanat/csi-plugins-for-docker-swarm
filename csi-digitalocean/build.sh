de493798f7c32de7470d0439fe2dceaf651c58d0
Thyran <benedikt-wbach@t-online.de>
2024-12-10 13:26:49 +0100

    added option to append platform to version

--- csi-digitalocean/build.sh
+++ csi-digitalocean/build.sh
@@ -9,12 +9,13 @@ fi
 
 ORG=$1
 VERSION=$2
+VERSION_PLATFORM=$2${PLUGIN_PLATFORM:+"-$PLUGIN_PLATFORM"}
 
 rm -rf rootfs
 docker plugin disable csi-digitalocean:latest
 docker plugin rm csi-digitalocean:latest
-docker plugin disable $ORG/swarm-csi-digitalocean:v$VERSION
-docker plugin rm $ORG/swarm-csi-digitalocean:v$VERSION
+docker plugin disable $ORG/swarm-csi-digitalocean:v$VERSION_PLATFORM
+docker plugin rm $ORG/swarm-csi-digitalocean:v$VERSION_PLATFORM
 docker rm -vf rootfsimage
 
 docker create --name rootfsimage docker.io/digitalocean/do-csi-plugin:v$VERSION
@@ -23,14 +24,14 @@ docker export rootfsimage | tar -x -C rootfs
 docker rm -vf rootfsimage
 cp entrypoint.sh rootfs/
 
-docker plugin create $ORG/swarm-csi-digitalocean:v$VERSION .
-docker plugin enable $ORG/swarm-csi-digitalocean:v$VERSION
-docker plugin push $ORG/swarm-csi-digitalocean:v$VERSION
-docker plugin disable $ORG/swarm-csi-digitalocean:v$VERSION
-docker plugin rm $ORG/swarm-csi-digitalocean:v$VERSION
+docker plugin create $ORG/swarm-csi-digitalocean:v$VERSION_PLATFORM .
+docker plugin enable $ORG/swarm-csi-digitalocean:v$VERSION_PLATFORM
+docker plugin push $ORG/swarm-csi-digitalocean:v$VERSION_PLATFORM
+docker plugin disable $ORG/swarm-csi-digitalocean:v$VERSION_PLATFORM
+docker plugin rm $ORG/swarm-csi-digitalocean:v$VERSION_PLATFORM
 docker plugin install \
     --alias csi-digitalocean \
     --grant-all-permissions \
-    $ORG/swarm-csi-digitalocean:v$VERSION \
+    $ORG/swarm-csi-digitalocean:v$VERSION_PLATFORM \
     DIGITALOCEAN_API_URL=$3 \
     DIGITALOCEAN_ACCESS_TOKEN=$4
--- csi-driver-aws-ebs/build.sh
+++ csi-driver-aws-ebs/build.sh
@@ -11,12 +11,13 @@ ORG=$1
 # 1.34.0 known to work
 # https://github.com/kubernetes-sigs/aws-ebs-csi-driver
 VERSION=$2
+VERSION_PLATFORM=$2${PLUGIN_PLATFORM:+"-$PLUGIN_PLATFORM"}
 
 rm -rf rootfs
 docker plugin disable csi-aws-ebs:latest
 docker plugin rm csi-aws-ebs:latest
-docker plugin disable $ORG/swarm-csi-aws-ebs:v$VERSION
-docker plugin rm $ORG/swarm-csi-aws-ebs:v$VERSION
+docker plugin disable $ORG/swarm-csi-aws-ebs:v$VERSION_PLATFORM
+docker plugin rm $ORG/swarm-csi-aws-ebs:v$VERSION_PLATFORM
 docker rm -vf rootfsimage
 
 docker create --name rootfsimage public.ecr.aws/ebs-csi-driver/aws-ebs-csi-driver:v$VERSION
@@ -24,9 +25,9 @@ mkdir -p rootfs
 docker export rootfsimage | tar -x -C rootfs
 docker rm -vf rootfsimage
 
-docker plugin create $ORG/swarm-csi-aws-ebs:v$VERSION .
-docker plugin enable $ORG/swarm-csi-aws-ebs:v$VERSION
-docker plugin push $ORG/swarm-csi-aws-ebs:v$VERSION
-docker plugin disable $ORG/swarm-csi-aws-ebs:v$VERSION
-docker plugin rm $ORG/swarm-csi-aws-ebs:v$VERSION
-docker plugin install --alias ecs-aws-ebs --grant-all-permissions $ORG/swarm-csi-aws-ebs:v$VERSION
+docker plugin create $ORG/swarm-csi-aws-ebs:v$VERSION_PLATFORM .
+docker plugin enable $ORG/swarm-csi-aws-ebs:v$VERSION_PLATFORM
+docker plugin push $ORG/swarm-csi-aws-ebs:v$VERSION_PLATFORM
+docker plugin disable $ORG/swarm-csi-aws-ebs:v$VERSION_PLATFORM
+docker plugin rm $ORG/swarm-csi-aws-ebs:v$VERSION_PLATFORM
+docker plugin install --alias ecs-aws-ebs --grant-all-permissions $ORG/swarm-csi-aws-ebs:v$VERSION_PLATFORM
